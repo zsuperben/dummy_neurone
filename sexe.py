a5913bf2d523b342edd693fe4947aad670199207
Benjamin Godard <bgodard@aortasoftware.com>
2015-11-30 19:57:15 +0100

    Unbreakable quality code

--- sexe.py
+++ sexe.py
@@ -8,30 +8,32 @@ values = []
 clf = None
 
 def digest():
-	global data, values, clf
-	clf = nn.MLPClassifier()
-	clf.fit(data, values)
+    global data, values, clf
+    clf = nn.MLPClassifier()
+    clf.fit(data, values)
 
 def compute(prenom):
-	return clf.predict(toVector(prenom))
+    return clf.predict(toVector(prenom))
 
 def toVector(prenom):
-	res = [0] * 100
-	i = 0
-	for c in prenom:
-		res[i] = ord(c)
-		i += 1
-	return res
+    res = [0] * 25
+    i = 0
+    for c in prenom:
+        if i >= 25:
+            break
+        res[i] = ord(c)
+        i += 1
+    return res
 
 def loadFile(fname, value):
-	global data, values
-	with open(fname, 'r') as f:
-		line = f.readline()
-		while line != '':
-			line = line[:-1]
-			data.append(toVector(line))
-			values.append(value)
-			line = f.readline()
+    global data, values
+    with open(fname, 'r') as f:
+        line = f.readline()
+        while line != '':
+            line = line[:-1]
+            data.append(toVector(line))
+            values.append(value)
+            line = f.readline()
 
 loadFile('filles.txt', True)
 loadFile('garcons.txt', False)
@@ -40,10 +42,10 @@ loadFile('garcons.txt', False)
 #print values
 digest()
 while True:
-	sys.stdout.write('Prenom : ')
-	prenom = sys.stdin.readline()[:-1]
+    sys.stdout.write('Prenom : ')
+    prenom = sys.stdin.readline()[:-1]
 
-	if compute(prenom):
-		print '%s est une fille' % prenom
-	else:
-		print '%s est un garcon' % prenom
+    if compute(prenom):
+        print '%s est une fille' % prenom
+    else:
+        print '%s est un garcon' % prenom
